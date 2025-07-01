4872cc1e8f4dd137ad8a81dfcdf99c9959e3d86e
Charles Shenkir <cshenkir@student.neumont.edu>
2025-07-01 10:19:33 -0600

    7/1 class

---
+++ Source/Engine/Core/Math.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace viper {
+	constexpr float pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286f;
+	constexpr float twopi = pi * 2;
+	constexpr float halfpi = pi / 2;
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,5 +1,12 @@
+#include "Core/Math.h"
+#include "../../Render/Renderer.h"
 #include <iostream>
+#define NAME "Ray\n"
+
 int main()
 {
+	viper::PrintMessage();
+	std::cout << NAME;
 	std::cout << "Hello, World!\n";
+	std::cout << viper::pi << std::endl;
 }
